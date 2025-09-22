70bc0450dc6e2eec97b94ccaf6ab507398e1ef95
gabushk4 <pereiralevesque.gabriel@gmail.com>
2025-09-22 15:25:13 -0400

    reparation pt II et implementation du la mise en page dynamique des annonces

---
+++ include/nav.php
@@ -0,0 +1,35 @@
+<nav class="nav-custom">
+    <div class="nav-I">
+            <a href="index.php">
+                <img src="./assets/brand.png" class="" href="./index.php">
+            </a>
+        <form class="barre-rech" role="search">
+            <input class="" type="search" placeholder="rechercher" aria-label="recherche"/>
+            <button class="btn-normal" type="submit">
+                <img src="./assets/rechercher.png" width="32" height="32" alt="loupe">
+            </button>
+        </form>
+    </div>
+    <ul class="nav-menu">
+        <li>
+            <a aria-current="page" href="#">vendre</a>
+        </li>
+        <li class="dropdown">
+            <a class="dropdown-button" onclick="toggleDropdown()">catégories ▼</a>
+            <ul class="dropdown-content" id="menuDropdown">                    
+                <li><a href="#">meubles</a></li>
+                <li><a href="#">appareils électroniques</a></li>
+                <li><a href="#">vêtements</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">à propos</a>
+        </li>
+    </ul>   
+    <div id="compte-btns">
+        <button class="btn-normal">connexion</button>
+        <div class="separateur"/>
+        <button class="btn-imp">inscription</button>
+    </div> 
+    <a id="hamburger">☰</a>            
+</nav>
--- index.php
+++ index.php
@@ -15,70 +15,56 @@
             <?php include "./include/nav.php" ?>
         </header>
         <main>
-            <div class="annonce">
-                <img src=""/>
-                <h2></h2>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-            </div>
-            <div class="annonce">
-                <img src=""/>
-                <h2></h2>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-            </div>
-            <div class="annonce">
-                <img src=""/>
-                <h2></h2>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-            </div>
-            <div class="annonce">
-                <img src=""/>
-                <h2></h2>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-            </div>
-            <div class="annonce">
-                <img src=""/>
-                <h2></h2>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-            </div>
-            <div class="annonce">
-                <img src=""/>
-                <h2></h2>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-            </div>
-            <div class="annonce">
-                <img src=""/>
-                <h2></h2>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-            </div>
-            <div class="annonce">
-                <img src=""/>
-                <h2></h2>
-                <p></p>
-                <p></p>
-                <p></p>
-                <p></p>
-            </div>
+            <?php
+                define("TITRE_I",0);
+                define("DESCRIPTION_I", 1);
+                define("PRIX_I",2);
+                define("NEGOCIABLE_I",3);
+                define("IMAGE_I",4);
+                define("AUTEUR_I",5);
+                @ $annoncesF = fopen('./data/annonces.csv', 'r');
+
+                if($annoncesF){
+                    while(!feof($annoncesF)){
+                        $annonce = fgetcsv($annoncesF, null, ':');
+                        if(is_array($annonce)){
+                            $negociable = $annonce[NEGOCIABLE_I] == "oui" ? "Négociable" : "Non négociable";
+                            echo '<div class="annonce">
+                                    <img src="./assets/'.$annonce[IMAGE_I].'"/>
+                                    <div class="annonce-texte-conteneur">
+                                        <h2>'.$annonce[TITRE_I].'</h2>
+                                        <h3>'.$annonce[DESCRIPTION_I].'</h3>
+                                        <div class="annonce-soustexte-conteneur">
+                                            <p>'.$annonce[PRIX_I].'$</p>
+                                            <p>'.$negociable.'</p>
+                                            <p>'.$annonce[AUTEUR_I].'</p>
+                                        </div>
+                                    </div>
+                                </div>';
+
+                        }else{
+                            echo <<<EOF
+                                <div class="annonce">
+                                    <img src=""/>
+                                    <h2>Annonce non chargée</h2>
+                                    <p>erreur</p>
+                                    <p>erreur</p>
+                                    <p>erreur</p>
+                                    <p>erreur</p>
+                                </div>
+                            EOF;
+                        }
+                        
+                    }
+                    fclose($annoncesF);
+                }else {
+                    echo <<<FIN
+                        Votre commande ne peut être traitée<br>
+                        Veuillez S.V.P. essayer plus tard
+                    FIN;
+                    exit(1); // termine immédiatement le programme
+                }
+            ?>
         </main>
     </body>
 </html>
